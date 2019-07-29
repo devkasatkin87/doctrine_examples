c93632b018c344f459faf1e59d2898c3cfbeff6d
root <devkasatkin87@gmail.com>
2019-07-29 18:58:47 +0000

    added Users Entity

---
+++ src/User.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @Entity @Table(name="users")
+ **/
+class User
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /** @Column(type="string") **/
+    protected $name;
+
+    /** @Column(type="string") **/
+    protected $email;
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
